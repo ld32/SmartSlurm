5e1b1c498a2616cae6e025eff8e583e0a176188e
Lingsheng Dong <lingsheng_dong@hms.harvard.edu>
2022-11-04 13:43:25 -0400

    try automatically get k

--- bin/textCluster.py
+++ bin/textCluster.py
@@ -4,6 +4,38 @@ import pandas as pd
 from sklearn.cluster import KMeans
 import numpy as np
 
+# from: https://towardsdatascience.com/an-approach-for-choosing-number-of-clusters-for-k-means-c28e614ecb2c
+def kMeansRes(scaled_data, k, alpha_k=0.02):
+    '''
+    Parameters 
+    ----------
+    scaled_data: matrix 
+        scaled data. rows are samples and columns are features for clustering
+    k: int
+        current k for applying KMeans
+    alpha_k: float
+        manually tuned factor that gives penalty to the number of clusters
+    Returns 
+    -------
+    scaled_inertia: float
+        scaled inertia value for current k           
+    '''
+    
+    inertia_o = np.square((scaled_data - scaled_data.mean(axis=0))).sum()
+    # fit k-means
+    kmeans = KMeans(n_clusters=k, random_state=0).fit(scaled_data)
+    scaled_inertia = kmeans.inertia_ / inertia_o + alpha_k * k
+    return scaled_inertia
+
+def chooseBestKforKMeans(scaled_data, k_range):
+    ans = []
+    for k in k_range:
+        scaled_inertia = kMeansRes(scaled_data, k)
+        ans.append((k, scaled_inertia))
+    results = pd.DataFrame(ans, columns = ['k','Scaled Inertia']).set_index('k')
+    best_k = results.idxmin()[0]
+    return best_k, results
+
 
 def cluster_text(text):
     from sklearn.feature_extraction.text import TfidfVectorizer
@@ -12,7 +44,7 @@ def cluster_text(text):
 
 
     Sum_of_squared_distances = []
-    K = range(2,5)
+    K = range(2,6)
     for k in K:
        print(k)
        km = KMeans(n_clusters=k, max_iter=200, n_init=10)
@@ -25,7 +57,20 @@ def cluster_text(text):
     #plt.show()
     plt.savefig("mygraph.png")
     
-   
+    df = pd.DataFrame(Sum_of_squared_distances)
+    
+    from sklearn.preprocessing import MinMaxScaler
+    mms = MinMaxScaler()
+    scaled_data = mms.fit_transform(df)
+
+    
+    
+    # choose k range
+    #k_range=range(2,6)
+    # compute adjusted intertia
+    best_k, results = chooseBestKforKMeans(scaled_data, K)
+
+    print(best_k)  
 
     print('How many clusters do you want to use?')
     true_k = int(input())
