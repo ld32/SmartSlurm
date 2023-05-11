6f8904388b5214924da5ee65fbe0c444cb0518d6
Lingsheng Dong <lingsheng_dong@hms.harvard.edu>
2023-05-11 08:06:32 -0400

    fix subsetFastq

--- bin/subsetFastq.sh
+++ bin/subsetFastq.sh
@@ -2,7 +2,7 @@
 
 #set -x 
 usage(){
-    echo -e "Usage:\nsubsetFastq.sh <path to original .fastq.gz file, such as: abc/x.fastq.gz, or the folder containing .fastq.gz files, such as: abc/fastq> <number of reads, such as: 500000 or 1000000>"; 
+    echo -e "Usage:\nsubsetFastq.sh <path to original .fastq.gz file, such as: abc/*.fastq.gz, or the folder containing .fastq.gz files, such as: abc/fastq> <number of reads, such as: 500000 or 1000000>"; 
     exit 1
 }
 
@@ -10,18 +10,22 @@ readCount="${@: -1}"
 
 test -n "$readCount" -a "$readCount" -ge 0 2>/dev/null || usage
 
-rowCount=$(( $readCount * 4 ))
+rowCount=$(( $readCount * 4000000 ))
+million=`echo "scale=6;$readCount/1000000"|bc| sed 's/^\./0./' | sed 's/0\{1,\}$//'`
 
+folder=fastq.$million.subset
+
+mkdir -p $folder
 for i in $@; do 
-    if [ -f $i ]; then 
-        if [[ "$i" == *fastq.gz ]]; then
-            [ -f ${i##*/} ] && echo Current working folder already have file ${i##*/}. Skipping it... && continue 
-            echo Working on $i ... && zcat $i  | head -n $rowCount | gzip > ${i##*/}; 
-        fi    
+    if [ -f $i ] && [[ "$j" == *fastq.gz ]]; then 
+        file=$folder/subset.$million.${i##*/}
+        [ -f "$file" ] && echo Current working folder already has file $file Skipping it... && continue 
+        echo Working on $j ...  && zcat $j  | head -n $rowCount | gzip > $file;    
     elif [ -d $i ]; then 
         for j in $i/*.fastq.gz; do 
-            [ -f ${j##*/} ] && echo Current working folder already have file ${j##*/}. Skipping it... && continue 
-            [ -f $j ] && [[ "$j" == *fastq.gz ]] && echo Working on $j ...  && zcat $j  | head -n $rowCount | gzip > ${j##*/}; 
+            file=$folder/subset.$million.${j##*/}
+            [ -f "$file" ] && echo Current working folder already has file $file Skipping it... && continue 
+            echo Working on $j ...  && zcat $j  | head -n $rowCount | gzip > $file; 
         done
     fi    
 done     
