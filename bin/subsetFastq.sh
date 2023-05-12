e2a2280d212745e30aee6c86b4f07307f881d054
Lingsheng Dong <lingsheng_dong@hms.harvard.edu>
2023-05-12 05:29:01 -0400

    subseqFastq

--- bin/subsetFastq.sh
+++ bin/subsetFastq.sh
@@ -12,18 +12,18 @@ test -n "$readCount" -a "$readCount" -ge 0 2>/dev/null || usage
 
 rowCount=$(( $readCount * 4000000 ))
 million=`echo "scale=6;$readCount/1000000"|bc| sed 's/^\./0./' | sed 's/0\{1,\}$//'`
-[[ million == 0* ]] && million="$million."
+[[ "$million" == 0* ]] && million="$million."
 folder=fastq.${million}subset
 
 mkdir -p $folder
 for i in $@; do 
     if [ -f $i ] && [[ "$j" == *fastq.gz ]]; then 
-        file=$folder/subset.$million.${i##*/}
+        file=$folder/subset.$million${i##*/}
         [ -f "$file" ] && echo Current working folder already has file $file Skipping it... && continue 
         echo Working on $j ...  && zcat $j  | head -n $rowCount | gzip > $file;    
     elif [ -d $i ]; then 
         for j in $i/*.fastq.gz; do 
-            file=$folder/subset.$million.${j##*/}
+            file=$folder/subset.$million${j##*/}
             [ -f "$file" ] && echo Current working folder already has file $file Skipping it... && continue 
             echo Working on $j ...  && zcat $j  | head -n $rowCount | gzip > $file; 
         done
