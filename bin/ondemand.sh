ffb4df06786122ac933c243132427ab9878d33f0
Lingsheng Dong <lingsheng_dong@hms.harvard.edu>
2023-09-21 13:31:07 -0400

    ondemand.sh

--- bin/ondemand.sh
+++ bin/ondemand.sh
@@ -1,60 +1,72 @@
+#!/bin/sh
+
+#set -x 
+
 user=$1 
-[ -z "$user" ] && { echo "Usage: ondemand.sh <USER> \nUsing sudo to find ondemand logs"; exit 1; }
+[ -z "$user" ] && { echo -e "Usage: ondemand.sh <USER> \nUsing sudo to find ondemand logs"; exit 1; }
 
 apps=`sudo ls /home/$user/ondemand/data/sys/dashboard/batch_connect/sys/`
 
-while [[ "$x: != "x"]]; then 
-    echo -e "Please select the app you want to check (type x to quit):" 
+while true; do
+    echo Availale apps with logs: 
     count=1
     for i in $apps; do 
         echo "$count $i" 
         count=$((count + 1))
     done 
+    echo -e "Please select the app you want to check (type q to quit):" 
     read -p "" x </dev/tty
 
-    [ $x =~ ^[1-9]$ ] || { echo "Out of range. Should be between 1 and $count;  exit 1; }
+    [[ "$x" == q ]] && break; 
+
+    [[ "$x" =~ ^[1-9]$ && "$x" -lt $count ]] || { echo "Out of range. Should be between > 0 and < $count"; continue; }
 
     app=`echo $apps | cut -d' ' -f$x`
 
-    sessions=`sudo ls -l /home/$user/ondemand/data/sys/dashboard/batch_connect/sys/$app/output`
+    sessions=`sudo ls -lrt /home/$user/ondemand/data/sys/dashboard/batch_connect/sys/$app/output | tail -n+2`
 
-    while [[ "$x: != "x"]]; then 
-
-        echo "Please select a session (type x to quit):" 
-
+    while true; do 
+        
+        echo Available sessions:
         count=1
         IFS=$'\n'
-        for i in `echo $sessions`; do
+        for i in $sessions; do
             echo $count: $i
             count=$((count + 1))
         done
+
+        echo -e "Please select a session (type q to quit):"
         read -p "" x </dev/tty
 
-        [ $x =~ ^[1-9]$ ] || { echo "Out of range. Should be between 1 and $count;  exit 1; }
+        [[ "$x" == q ]] && break;
 
+        [[ "$x" =~ ^[1-9]$ && "$x" -lt $count ]] || { echo "Out of range. Should be > 0 and < $count";  continue; }
 
-        session=`echo $sessions | cut -d'\n' -f$x 
-
+        session=`echo -e "$sessions" | head -n $x | tail -n1 | awk '{print $NF}'`
+        
+        echo jobInfo: 
+        
+        sudo less /home/$user/ondemand/data/sys/dashboard/batch_connect/db/$session 
+        
         files=`sudo ls /home/$user/ondemand/data/sys/dashboard/batch_connect/sys/$app/output/$session`
 
-        while [[ "$x: != "x"]]; then 
-            echo "Please select a file (type x to quit):" 
+        while true; do 
+             
             count=1
-            IFS=$' '
-            for i in `echo $files`; do
+            echo Available files:
+            for i in $files; do
                 echo $count: $i
                 count=$((count + 1))
             done
+            echo -e "Please select a file to view (type q to quit):"
             read -p "" x </dev/tty
 
-            [ $x =~ ^[1-9]$ ] || { echo "Out of range. Should be between 1 and $count;  exit 1; }
+            [[ "$x" == q ]] && break;
+            [[ "$x" =~ ^[1-9]$ && "$x" -lt $count ]] || { echo "Out of range. Should be > 0 and < $count";  continue; }
 
-
-            file=`echo $files | cut -d' ' -f$x
+            file=`echo $files | cut -d' ' -f$x`
 
             sudo less /home/$user/ondemand/data/sys/dashboard/batch_connect/sys/$app/output/$session/$file
         done    
     done
 done
-
-        
