74a1177943ea59e567b54cf12d049b046a9988bd
Lingsheng Dong <lingsheng_dong@hms.harvard.edu>
2023-10-05 12:44:07 -0400

    re-arrange folder

--- scripts/eclipV2.sh
+++ scripts/eclipV2.sh
@@ -397,7 +397,7 @@ for dir1 in `ls -v -d smartSlurmInputs/*/`; do
         --direction r
 
         #Clipper:  Takes results from samtools view.  Calls peaks on those files.  
-        #@16,15,cliper,,,sbatch -c 1 -p short -t 2:0:0 --mem 8G 
+        #@16,15,cliper,,,sbatch -c 1 -p short -t 12:0:0 --mem 8G 
         sh -c "conda deactivate; source activate /n/data1/cores/bcbio/eclip/clipperEnv; \
         clipper --species hg19 \
         --bam EXAMPLE_PE.rep2_clip.r1.fq.genome-mappedSo.rmDupSo.merged.r2.bam \
